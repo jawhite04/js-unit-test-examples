5bfe2e5804e73008a9a09913e188e55bcf544344
John White <jawhite04@gmail.com>
2021-07-12 23:55:02 -0400

    code coverage updates

--- nyc.config.js
+++ nyc.config.js
@@ -1,9 +1,9 @@
 const config = {
   'check-coverage': true,
-  branches: 80,
-  lines: 80,
-  functions: 80,
-  statements: 80,
+  branches: 100,
+  lines: 100,
+  functions: 100,
+  statements: 100,
 
   include: ['**/src/**/*.js'],
   exclude: ['**/test/**/*.spec.js'],
