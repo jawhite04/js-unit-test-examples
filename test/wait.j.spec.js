ae442e8fd1619db07e7b123e56721ce1b07b4d0d
John White <jawhite04@gmail.com>
2021-07-17 19:57:10 -0400

    setTimeout example + tests

---
+++ test/wait.j.spec.js
@@ -0,0 +1,39 @@
+describe('./src/wait.js', () => {
+  const mockLogger = {
+    info: jest.fn(),
+  };
+  jest.mock('@/src/common/logger.js', () => mockLogger);
+
+  describe('ms', () => {
+    // i had a hard time using jest.useFakeTimers to mock setTimeout
+    const setTimeoutSpy = jest.spyOn(global, 'setTimeout');
+    setTimeoutSpy.mockImplementation((fn) => fn());
+
+    const wait = require('@/src/wait');
+
+    it('calls setTimeout with a time to wait', async () => {
+      // arrange
+      // jest's default timeout is 5000 ms, so let's set this
+      // longer so that any failure to mock will be apparent.
+      // jest.setTimeout is not valid here, increasing the timeout
+      // of this test would be trying to solve the wrong problem.
+      const msToWait = 6000;
+
+      // act
+      await wait.ms(msToWait);
+
+      // assert
+      expect(setTimeoutSpy).toHaveBeenCalledTimes(1);
+      expect(setTimeoutSpy).toHaveBeenCalledWith(expect.any(Function), msToWait);
+    });
+
+    it('calls setTimeout without a time to wait', async () => {
+      // act
+      await wait.ms();
+
+      // assert
+      expect(setTimeoutSpy).toHaveBeenCalledTimes(1);
+      expect(setTimeoutSpy).toHaveBeenCalledWith(expect.any(Function), 0);
+    });
+  });
+});
---
+++ test/wait.m.spec.js
@@ -0,0 +1,43 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+const wait = require('../src/wait');
+
+describe('./src/wait.js', () => {
+  let clock;
+  let setTimeoutSpy;
+  before(() => {
+    clock = sinon.useFakeTimers();
+    setTimeoutSpy = sinon.spy(clock, 'setTimeout');
+  });
+  afterEach(() => {
+    setTimeoutSpy.resetHistory();
+  });
+  after(() => {
+    clock.restore();
+  });
+
+  it('calls setTimeout with a time to wait', async () => {
+    // arrange
+    const msToWait = 6000;
+
+    // act
+    const responsePromise = wait.ms(msToWait);
+    clock.tick(msToWait);
+    await responsePromise;
+
+    // assert
+    sinon.assert.callCount(setTimeoutSpy, 1);
+    expect(setTimeoutSpy.calledWith(sinon.match.func, msToWait)).to.equal(true);
+  });
+
+  it('calls setTimeout with a default', async () => {
+    // act
+    const responsePromise = wait.ms();
+    clock.tick(10);
+    await responsePromise;
+
+    // assert
+    sinon.assert.callCount(setTimeoutSpy, 1);
+    expect(setTimeoutSpy.calledWith(sinon.match.func, 0)).to.equal(true);
+  });
+});
